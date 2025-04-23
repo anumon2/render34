66f09eba989a704c4b9929237f3c40f1f5161bf6
anumon2 <148676346+anumon2@users.noreply.github.com>
2025-04-23 22:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+// Use the dynamic port provided by the hosting environment
+const port = process.env.PORT || 1880;
 
 app.use(express.json());
 
+// Endpoint to turn on the light
 app.get('/lighton', (req, res) => {
   console.log('Light ON command received');
   res.send('Light turned on');
 });
 
+// Endpoint to turn off the light
 app.get('/lightoff', (req, res) => {
   console.log('Light OFF command received');
   res.send('Light turned off');
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
-
-
-
